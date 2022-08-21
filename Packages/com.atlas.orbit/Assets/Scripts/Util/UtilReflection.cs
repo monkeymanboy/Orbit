70ee5fb0ccd686150dd5357a007710249fe1a46e
dcljr <dcljr@me.com>
2022-08-21 15:10:00 -0700

    Add GetAllSubclasses in external assemblies

--- Packages/com.atlas.orbit/Assets/Scripts/Util/UtilReflection.cs
+++ Packages/com.atlas.orbit/Assets/Scripts/Util/UtilReflection.cs
@@ -12,8 +12,18 @@ namespace Atlas.Orbit.Util {
         /// <param name="constructorArgs">Arguments to pass to the constructors of each instance</param>
         /// <returns>List of instances</returns>
         public static List<T> GetAllSubclasses<T>(params object[] constructorArgs) {
+            return GetAllSubclasses<T>(Assembly.GetAssembly(typeof(T)));
+        }
+        /// <summary>
+        /// Creates a list of instances of every subclass of a certain type
+        /// </summary>
+        /// <typeparam name="T">Parent type</typeparam>
+        /// <param name="assembly">Assembly to search for types in</param>
+        /// <param name="constructorArgs">Arguments to pass to the constructors of each instance</param>
+        /// <returns>List of instances</returns>
+        public static List<T> GetAllSubclasses<T>(Assembly assembly, params object[] constructorArgs) {
             List<T> objects = new List<T>();
-            foreach(Type type in Assembly.GetAssembly(typeof(T)).GetTypes().Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(T))))
+            foreach(Type type in assembly.GetTypes().Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(T))))
                 objects.Add((T)Activator.CreateInstance(type, constructorArgs));
 
             return objects;
