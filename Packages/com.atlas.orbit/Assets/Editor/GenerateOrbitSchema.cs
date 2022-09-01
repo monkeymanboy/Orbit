982c50555c71f44ccae34e170a6933658afcf377
dcljr <dcljr@me.com>
2022-09-01 14:22:25 -0700

    Fixed MenuItem preventing builds

--- Packages/com.atlas.orbit/Assets/Editor/GenerateOrbitSchema.cs
+++ Packages/com.atlas.orbit/Assets/Editor/GenerateOrbitSchema.cs
@@ -2,8 +2,10 @@ using Atlas.Orbit.Schema;
 using UnityEditor;
 
 public static class GenerateOrbitSchema {
+    #if UNITY_EDITOR
     [MenuItem("Tools/Generate Orbit Schema")]
     private static void Generate() {
         SchemaGenerator.Generate();
     }
+    #endif
 }
