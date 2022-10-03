8e97981bb5f40ededec66be4bb59e9aff03ed448
Caeden117 <caeden.s@outlook.com>
2022-10-02 19:30:53 -0700

    Add UNITY_EDITOR check to GenerateOrbitSchema

--- Packages/com.atlas.orbit/Assets/Editor/GenerateOrbitSchema.cs
+++ Packages/com.atlas.orbit/Assets/Editor/GenerateOrbitSchema.cs
@@ -2,7 +2,7 @@ using Atlas.Orbit.Schema;
 using UnityEditor;
 
 public static class GenerateOrbitSchema {
-    #if !OMIT_ORBIT_SCHEMA_GEN
+    #if !OMIT_ORBIT_SCHEMA_GEN && UNITY_EDITOR
     [MenuItem("Tools/Generate Orbit Schema")]
     private static void Generate() {
         SchemaGenerator.Generate();
