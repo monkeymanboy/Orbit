b5654d6d5ff775ee9c5aae4bdd326a6bf2dfbf09
dcljr <dcljr@me.com>
2022-11-10 15:14:43 -0800

    Refresh ForEachMacro on host change

--- Packages/com.atlas.orbit/Assets/Scripts/Macros/ForEachMacro.cs
+++ Packages/com.atlas.orbit/Assets/Scripts/Macros/ForEachMacro.cs
@@ -13,6 +13,7 @@ namespace Atlas.Orbit.Macros {
     public class ForEachMacro : Macro<ForEachMacro> {
         public override string Tag => "FOR_EACH";
 
+        public UIValue ItemsValue { get; set; }
         public IList Items { get; set; }
         public string RefreshEvent { get; set; }
 
@@ -21,28 +22,37 @@ namespace Atlas.Orbit.Macros {
             {"RefreshEvent", new StringSetter<ForEachMacro>((data,value) => data.RefreshEvent = value)}
         };
 
+        public override Dictionary<string, TypeSetter<ForEachMacro, UIValue>> ValueSetters=> new() {
+            {"Items", new ObjectSetter<ForEachMacro, UIValue>((data, value) => data.ItemsValue = value) },
+        };
+
         public override void Execute(XmlNode node, GameObject parent, ForEachMacro data) {
+            UIValue uiValue = ItemsValue;
             IList items = data.Items;
             UIRenderData renderData = CurrentData;
             List<UIRenderData> rendered = new(items.Count);
             for(int i=0;i<items.Count;i++) {
                 rendered.Add(Parser.Parse(node, parent, items[i], CurrentData));
             }
+            Action refreshAction = () => {
+                for(int i=0;i<items.Count;i++) {
+                    if(i < rendered.Count) {
+                        rendered[i].EnableRootObjects();
+                        rendered[i].Host = items[i];
+                    } else {
+                        rendered.Add(Parser.Parse(node, parent, items[i], renderData));
+                    }
+                }
+                for(int i = items.Count;i < rendered.Count;i++) {
+                    rendered[i].DisableRootObjects();
+                }
+            };
+            ItemsValue.OnChange += () => {
+                items = uiValue.GetValue<IList>();
+                refreshAction();
+            };
 
             if(RefreshEvent != null) {
-                Action refreshAction = () => {
-                    for(int i=0;i<items.Count;i++) {
-                        if(i < rendered.Count) {
-                            rendered[i].EnableRootObjects();
-                            rendered[i].Host = items[i];
-                        } else {
-                            rendered.Add(Parser.Parse(node, parent, items[i], renderData));
-                        }
-                    }
-                    for(int i = items.Count;i < rendered.Count;i++) {
-                        rendered[i].DisableRootObjects();
-                    }
-                };
                 renderData.AddEvent(RefreshEvent, refreshAction);
             }
         }
