a6b7e213e7dc7751f9df7af37c07258f906ef9a0
Caeden Statia <caeden.s@outlook.com>
2022-11-10 17:47:04 -0800

    Expose ScrollRect properties

---
+++ Packages/com.atlas.orbit/Assets/Scripts/ComponentProcessors/ScrollRectProcessor.cs
@@ -0,0 +1,17 @@
+﻿using Atlas.Orbit.TypeSetters;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Atlas.Orbit.ComponentProcessors {
+    public class ScrollRectProcessor : ComponentProcessor<ScrollRect> {
+        public override Dictionary<string, TypeSetter<ScrollRect>> Setters => new() {
+            { "MovementType", new EnumSetter<ScrollRect, ScrollRect.MovementType>((component, value) => component.movementType = value) },
+            { "Elasticity", new FloatSetter<ScrollRect>((component, value) => component.elasticity = value) },
+            { "Horizontal", new BoolSetter<ScrollRect>((component, value) => component.horizontal = value) },
+            { "Vertical", new BoolSetter<ScrollRect>((component, value) => component.vertical = value) },
+            { "Inertia", new BoolSetter<ScrollRect>((component, value) => component.inertia = value) },
+            { "DecelerationRate", new FloatSetter<ScrollRect>((component, value) => component.decelerationRate = value) },
+            { "ScrollSensitivity", new FloatSetter<ScrollRect>((component, value) => component.scrollSensitivity = value) },
+        };
+    }
+}
