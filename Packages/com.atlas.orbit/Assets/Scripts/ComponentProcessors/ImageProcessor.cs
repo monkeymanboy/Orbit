c4f542dc64cf9cab32170ca4c05c51cba036a153
dcljr <dcljr@me.com>
2023-07-06 10:52:19 -0700

    Added ImageMaterial property

--- Packages/com.atlas.orbit/Assets/Scripts/ComponentProcessors/ImageProcessor.cs
+++ Packages/com.atlas.orbit/Assets/Scripts/ComponentProcessors/ImageProcessor.cs
@@ -10,6 +10,7 @@ namespace Atlas.Orbit.ComponentProcessors {
         public override Dictionary<string, TypeSetter<Image>> Setters => new() {
             {"ImageColor", new ColorSetter<Image>((component, value) => component.color = value) },
             {"ImageSprite", new ObjectSetter<Image, Sprite>((component, value) => component.sprite = value) },
+            {"ImageMaterial", new ObjectSetter<Image, Material>((component, value) => component.material = value) },
             {"PreserveAspect", new BoolSetter<Image>((component, value) => component.preserveAspect = value) },
         };
     }
