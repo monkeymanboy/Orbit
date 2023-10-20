4da2e49654a0da15fb7a794b1fba88e302b80672
dcljr <dcljr@me.com>
2023-10-19 17:26:43 -0700

    Propagate value changes when as host macro host value changes

--- Packages/com.atlas.orbit/Assets/Scripts/Macros/AsHostMacro.cs
+++ Packages/com.atlas.orbit/Assets/Scripts/Macros/AsHostMacro.cs
@@ -4,6 +4,7 @@ using Atlas.Orbit.TypeSetters;
 using UnityEngine;
 
 namespace Atlas.Orbit.Macros {
+    using Parser;
     using Schema.Attributes;
     using TypeSetters;
 
@@ -12,17 +13,27 @@ namespace Atlas.Orbit.Macros {
         public override string Tag => "AS_HOST";
 
         public object Host { get; set; }
+        public UIValue HostValue { get; set; }
 
-        public override Dictionary<string, TypeSetter<AsHostMacro>> Setters => new Dictionary<string, TypeSetter<AsHostMacro>>() {
+        public override Dictionary<string, TypeSetter<AsHostMacro>> Setters => new() {
             {"Host", new ObjectSetter<AsHostMacro, object>((data, value) => data.Host = value) },
         };
 
+        public override Dictionary<string, TypeSetter<AsHostMacro, UIValue>> ValueSetters => new() { 
+            {"Host", new ObjectSetter<AsHostMacro, UIValue>((data, value) => data.HostValue = value) },
+        };
+
         public override void Execute(XmlNode node, GameObject parent, AsHostMacro data) {
-            Parser.Parse(node, parent, Host, CurrentData);
+            UIRenderData renderData = Parser.Parse(node, parent, Host, CurrentData);
+            UIValue hostValue = HostValue;
+            if(hostValue != null) {
+                hostValue.OnChange += () => renderData.Host = hostValue.GetValue();
+            }
         }
 
         public override void SetToDefault() {
             Host = null;
+            HostValue = null;
         }
     }
 }
