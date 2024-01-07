8b5b4225eccb7ca51cb94ab83a6becab2409cb9f
dcljr <dcljr@me.com>
2024-01-07 14:14:48 -0800

    Add AutoFontSize property

--- Packages/com.atlas.orbit/Assets/Scripts/ComponentProcessors/TextMeshProUGUIProcessor.cs
+++ Packages/com.atlas.orbit/Assets/Scripts/ComponentProcessors/TextMeshProUGUIProcessor.cs
@@ -1,14 +1,15 @@
-﻿using Atlas.Orbit.TypeSetters;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TMPro;
 
 namespace Atlas.Orbit.ComponentProcessors {
     using TypeSetters;
+    using UnityEngine;
 
     public class TextMeshProUGUIProcessor : ComponentProcessor<TextMeshProUGUI> {
         public override Dictionary<string, TypeSetter<TextMeshProUGUI>> Setters => new() {
             {"Text", new StringSetter<TextMeshProUGUI>((component, value) => component.text = value) },
             {"FontSize", new FloatSetter<TextMeshProUGUI>((component, value) => component.fontSize = value) },
+            {"AutoFontSize", new Vector2Setter<TextMeshProUGUI>(SetAutoFontSize) },
             {"FontColor", new ColorSetter<TextMeshProUGUI>((component, value) => component.color = value) },
             {"RichText", new BoolSetter<TextMeshProUGUI>((component, value) => component.richText = value) },
             {"FontStyle", new EnumSetter<TextMeshProUGUI, FontStyles>((component, value) => component.fontStyle = value) },
@@ -16,5 +17,11 @@ namespace Atlas.Orbit.ComponentProcessors {
             {"TextOverflowMode", new EnumSetter<TextMeshProUGUI, TextOverflowModes>((component, value) => component.overflowMode = value) },
             {"WordWrapping", new BoolSetter<TextMeshProUGUI>((component, value) => component.enableWordWrapping = value) },
         };
+
+        private void SetAutoFontSize(TextMeshProUGUI component, Vector2 size) {
+            component.fontSizeMin = size.x;
+            component.fontSizeMax = size.y;
+            component.enableAutoSizing = true;
+        }
     }
 }
